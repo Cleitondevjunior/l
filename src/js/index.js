c82c991b054074ce78753c9436789bba2f084bf1
Cleiton lima de Andrade <158637507+Cleitondevjunior@users.noreply.github.com>
2024-04-14 22:49:20 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+
+
+
+botaoMostrarProjetos.addEventListener('click', () => {
+
+    mostrarMaisprojetos();
+
+
+
+    esconderBotao();
+
+    
+});
+
+
+function esconderBotao() {
+    botaoMostrarProjetos.classList.add('remove');
+}
+
+function mostrarMaisprojetos() {
+    projetosInativos.forEach(projetosInativo => {
+        projetosInativo.classList.add('ativo');
+    });
+}
+
